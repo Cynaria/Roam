26edbba23bb9fc2751416cf5ada42ed8e5eb5755
Cynaria <Elizabeth.J.Cruz@gmail.com>
2015-07-30 15:31:26 -0500

    Added create and destroy to Sessions

--- roam-api/app/controllers/api/sessions_controller.rb
+++ roam-api/app/controllers/api/sessions_controller.rb
@@ -1,15 +1,17 @@
 class Api::SessionsController < Devise::SessionsController
-	def create
-    respond_to do |format|
-      format.json do
-        self.resource = warden.authenticate!(auth_options)
-        sign_in(resource_name, resource)
-        data = {
-          token: self.resource.authentication_token,
-          email: self.resource.email
-        }
-        render json: data, status: 201
-      end
-    end
+	respond_to :json
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+    resource.reset_authentication_token!
+    resource.save!
+    render json: {
+      auth_token: resource.reset_authentication_token,
+      user_role: resource.role
+    }
+  end
+
+  def destroy
+    sign_out(resource_name)
   end
 end
